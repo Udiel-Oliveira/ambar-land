a924d276240805f00d3e554f41e62c175757ba24
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2025-03-30 08:59:55 -0300

    fix: tempo de carregamento

--- Js/main.js
+++ Js/main.js
@@ -7,7 +7,7 @@ window.addEventListener('load', function() {
     setTimeout(function() {
         loadingScreen.style.display = 'none';
         content.style.display = 'block';
-    }, 3000);
+    }, 30);
 }); 
 
 const video = document.getElementById("meu-video");
